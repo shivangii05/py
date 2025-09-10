e20269516a6099d3971789f68f18766b5e71f07f
Shivangi Singh <24f3003422@ds.study.iitm.ac.in>
2025-09-10 11:44:43 +0530

    Update2 test.py

--- Basics/test.py
+++ Basics/test.py
@@ -1,6 +1,4 @@
 def sum(a,b):
     return a+b
-
-n1=int(input('enter first number: '))
-n2=int(input('enter first number: '))
-print('sum is: ',sum(n1,n2))
+n1, n2 = map(int, input("Enter the numbers: ").split())
+print("The sum is: ",sum(n1, n2))
